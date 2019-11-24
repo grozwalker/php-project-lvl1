666ba619b38b63f1276c7459623f270c59fdad74
Andrey Groza <andrey.groza@mail.ru>
2019-11-24 14:19:21 +0300

    add game

---
+++ src/app.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BrainGames\App;
+
+function isEven(int $number): bool
+{
+    return $number % 2 ===0;
+}
--- src/cli.php
+++ src/cli.php
@@ -2,12 +2,43 @@
 
 namespace BrainGames\Cli;
 
+use function BrainGames\App\isAnswerCorrect;
+use function BrainGames\App\isEven;
 use function cli\line;
 use function cli\prompt;
 
+const NUMBER_OF_QUESTIONS = 3;
+
 function run()
 {
+
     line('Welcome to the Brain Game!');
+    line('Answer "yes" if number even otherwise answer "no".');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+
+    $isCorrectAnswer = false;
+
+    for ($i = 0; $i < NUMBER_OF_QUESTIONS; $i++) {
+        $number = mt_rand(1, 99);
+        line('Question: ' . $number);
+
+        $userAnswer = prompt('Your answer');
+        $correctAnswer = isEven($number) ? 'yes' : 'no';
+
+        $isCorrectAnswer = $userAnswer === $correctAnswer;
+
+        if ($isCorrectAnswer) {
+            line('Correct!');
+        } else {
+            line($userAnswer . ' is wrong answer ;(. Correct answer was ' . $correctAnswer);
+            break;
+        }
+    }
+
+    if ($isCorrectAnswer) {
+        line("Congratulations, %s!", $name);
+    } else {
+        line("Let's try again, %s!", $name);
+    }
 }
