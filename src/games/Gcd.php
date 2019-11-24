785771725488669e30cf675762529d79fd6fd87d
Andrey Groza <andrey.groza@mail.ru>
2019-11-24 22:14:28 +0300

    add gcd

---
+++ src/games/Gcd.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BrainGames\Games\Gcd;
+
+const GAME_NAME = 'Find the greatest common divisor of given numbers.';
+
+function getConditionals()
+{
+    $firstTerm = mt_rand(1, 99);
+    $secondTerm = mt_rand(1, 99);
+
+
+    return [
+        (string) $firstTerm . ' ' . (string) $secondTerm,
+        [
+            'firstTerm' => $firstTerm,
+            'secondTerm' => $secondTerm,
+        ]
+    ];
+}
+
+function getAnswer(array $conditionals)
+{
+    [
+        'firstTerm' => $firstTerm,
+        'secondTerm' => $secondTerm
+    ] = $conditionals;
+
+    $correctAnswer = getGcd($firstTerm, $secondTerm);
+
+    return $correctAnswer;
+}
+
+function getGcd($firstTerm, $secondTerm)
+{
+    while (true) {
+        if ($firstTerm === $secondTerm) {
+            return $secondTerm;
+        }
+
+        if ($firstTerm > $secondTerm) {
+            $firstTerm -= $secondTerm;
+        } else {
+            $secondTerm -= $firstTerm;
+        }
+    }
+}
